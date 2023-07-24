9c733d712e1e8275a78d731f5dc295c917e6baef
kochrist66 <kochrist66@gmail.com>
2023-07-24 09:33:03 +0900

    java first commit

---
+++ javaPrj/src/algoPrj/day1/박태민.java
@@ -0,0 +1,15 @@
+package algoPrj.day1;
+
+public class 박태민 {
+	
+	public static void main(String[] args) {
+		
+		int sum=0;
+		for ( int n=0; n<100; n++) {
+			sum = sum + n*(101-n);
+			
+		}
+		System.out.println(sum);
+	}
+
+}
